9ae810af9731f796e6f61178bc3a90e7a995fb15
Vivek3927 <vivek.vas27@gmail.co>
2021-08-29 23:39:56 +0530

    crud completed with AJAX

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     respond_to do |format|
-      if verify_recaptcha(model: @user) && @user.save
+      # if verify_recaptcha(model: @user) && @user.save
+      if @user.save
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
         format.js
