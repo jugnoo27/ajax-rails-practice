0503bd2ba04acf05c0bc540a3530aa3734291dca
Vivek3927 <vivek.vas27@gmail.co>
2021-09-26 23:41:26 +0530

    ajax crud for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+
+  resources :products
+  resources :articles
   resources :users
   root 'welcome#index'
 end
